fe6ae301c6b0f04eb282bc68c4c470f9025d4361
AFRIAULAR <aularafrica@gmail.com>
2021-02-07 23:15:27 -0300

    estilos y js, formato de texto

--- js/app.js
+++ js/app.js
@@ -15,6 +15,17 @@ const bottomText = document.getElementById('bottomText');
 const bottomTextCheck = document.getElementById('bottomTextCheck');
 
 const fontType = document.getElementById('option');
+const fontSize = document.getElementById ('fontSize');
+const textAlignCenter = document.getElementById ('center');
+const textAlignRight = document.getElementById ('right');
+const textAlignLeft = document.getElementById ('left');
+const fontColor = document.getElementById ('color-text');
+const colorBack = document.getElementById ('color-background');
+const transparentBack = document.getElementById ('transparentBack')
+
+const spacing = document.getElementById ('spacing');
+const lineSpacing = document.getElementById ('lineSpacing');
+
 
 /*        *****EVENTOS DE FORMULARIO DE TEXTO*****    */
 
@@ -46,32 +57,105 @@ bottomTextCheck.addEventListener('click', () =>{
 
 
 /* FUENTES */
-
 fontType.addEventListener('change',()=>{
     articleTopText.style.fontFamily = `${option.value}`;
     articleBottomText.style.fontFamily = `${option.value}`;
 });   /* el evento funciona pero no las fontfamily*/ 
 
-const fontSize = document.getElementById ('fontSize');
 fontSize.addEventListener('change', () =>{
     articleTopText.style.fontSize = `${fontSize.value}px`;
     articleBottomText.style.fontSize = `${fontSize.value}px`;
 });
 
- const textAlignCenter = document.getElementById ('center');
-textAlignCenter.addEventListener('click',()=>{
+textAlignCenter.addEventListener('click',(e)=>{
+    e.preventDefault();
     articleTopText.style.textAlign = 'center';
     articleBottomText.style.textAlign = 'center';
 });
 
-const textAlignRight = document.getElementById ('right');
-textAlignRight.addEventListener('click',()=>{
+textAlignRight.addEventListener('click',(e)=>{
+    e.preventDefault();
     articleTopText.style.textAlign = 'right';
     articleBottomText.style.textAlign = 'right';
 }); 
 
-const textAlignLeft = document.getElementById ('left');
-textAlignLeft.addEventListener('click',()=>{
+textAlignLeft.addEventListener('click',(e)=>{
+    e.preventDefault();
     articleTopText.style.textAlign = 'left';
     articleBottomText.style.textAlign = 'left';
+});   
+
+fontColor.addEventListener('input', (e) => {
+    articleTopText.style.color = e.target.value;
+    articleBottomText.style.color = e.target.value;
+    fontColor.innerText = `${e.target.value}`;
 });
+
+colorBack.addEventListener('input', (e) => {
+    articleTopText.style.backgroundColor = e.target.value;
+    articleBottomText.style.backgroundColor = e.target.value;
+    colorBack.innerText = `${e.target.value}`;
+});
+
+transparentBack.addEventListener('change', () => {
+
+    if(transparentBack.checked){
+        articleTopText.style.backgroundColor = 'transparent';
+        articleTopText.style.position = 'absolute';
+        articleTopText.style.top = '0';
+        articleBottomText.style.backgroundColor = 'transparent';
+        articleBottomText.style.position = 'absolute';
+        articleBottomText.style.bottom = '0';
+
+    }else{
+        articleTopText.style.backgroundColor = `${colorBack.value}`;
+        articleTopText.style.position = 'relative';
+        articleBottomText.style.backgroundColor = `${colorBack.value}`;
+        articleBottomText.style.position = 'relative';
+    }
+});    
+
+
+/*VER POSICION DE BOTTOM TEXT*/
+
+/*   ******CONTORNOS!!!  NO FUNCIONA******
+const nonCont = document.getElementById ('non-cont');
+    nonCont.addEventListener ('click', (e) =>{
+        e.preventDefault();
+        articleTopText.classList.remove('claire-cont');
+        articleTopText.classList.remove('dark-cont');
+        articleBottomText.classList.remove('claire-cont');
+        articleBottomText.classList.remove('dark-cont');
+    });
+
+const claireCont = document.getElementById ('claire-cont');
+    claireCont.addEventListener ('click', (e) =>{
+        e.preventDefault();
+        articleTopText.classList.add('claire-cont');
+        articleBottomText.classList.add('claire-cont');
+    });
+
+const darkCont = document.getElementById ('dark-cont');
+    darkCont.addEventListener ('click', (e) =>{
+        e.preventDefault();
+        articleTopText.classList.add('dark-cont');
+        articleBottomText.classList.add('dark-cont');
+    });
+*/
+
+
+spacing.addEventListener ('change', (e) =>{
+    articleTopText.style.padding = `${e.target.value}px 20px`;
+    articleBottomText.style.padding = `${e.target.value}px 20px`;
+    articleBottomText.style.marginTop = '0';
+    })
+
+
+lineSpacing.addEventListener('change', (e) =>{
+    articleTopText.style.lineHeight = `${e.target.value}`;
+    articleBottomText.style.lineHeight = `${e.target.value}`;
+    articleBottomText.style.marginTop = '0';
+});
+
+
+
